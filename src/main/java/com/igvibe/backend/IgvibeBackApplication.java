249551c86ff87fdcb1767880ffb11cf1772fe40a
dlsrjf1107 <dlsrjf1107@naver.com>
2025-03-06 17:58:45 +0900

    igvibe initial springboot setting

---
+++ src/main/java/com/igvibe/backend/IgvibeBackApplication.java
@@ -0,0 +1,13 @@
+package com.igvibe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgvibeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgvibeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igvibe/backend/IgvibeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igvibe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgvibeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
