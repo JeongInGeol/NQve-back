21e362fa525e6b80fa237dc656be4c29fdfefaf8
InGeol Jeong <dlsrjf1107@gmail.com>
2025-05-01 22:05:40 +0900

    [google oAuth2.0 개발 시작]
    #1. application.yml 에 DB 접속 정보 수정
    #2. gradle 에 bootRun 옵션 추가 (보안을 위해서 .env에서 google client id랑 client secret 키 가져오기)
    #3. Test class 명 수정

--- src/test/java/com/igvibe/backend/IgvibeBackApplicationTests.java
+++ src/test/java/com/igvibe/backend/IgvibeBackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IgvibeBackApplicationTests {
+class NQveBackApplicationTests {
 
 	@Test
 	void contextLoads() {
